8edef7a5db316d3cb814851b70fd2ccbba7abc28
TheMrTiky <95758136+TheMrTiky@users.noreply.github.com>
2022-01-04 14:31:42 +0000

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -372,8 +372,6 @@ class PlayState extends MusicBeatState
 				var bg:BGSprite = new BGSprite('arenasinh', -600, -200, 0.9, 0.9);
 				add(bg);
 
-				}
-
 			case 'spooky': //Week 2
 				if(!ClientPrefs.lowQuality) {
 					halloweenBG = new BGSprite('halloween_bg', -200, -100, ['halloweem bg0', 'halloweem bg lightning strike']);
