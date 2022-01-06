9660add0b45a66d9d1c1a0469578f3c56e1253e4
TheMrTiky <95758136+TheMrTiky@users.noreply.github.com>
2022-01-06 11:28:16 +0000

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -729,6 +729,10 @@ class PlayState extends MusicBeatState
 			case 'schoolEvil':
 				var evilTrail = new FlxTrail(dad, null, 4, 24, 0.3, 0.069); //nice
 				insert(members.indexOf(dadGroup) - 1, evilTrail);
+
+			case 'arenasing':
+				var evilTrail = new FlxTrail(dad, null, 4, 24, 0.3, 0.069); //nice
+				insert(members.indexOf(dadGroup) - 1, evilTrail);
 		}
 
 		var file:String = Paths.json(songName + '/dialogue'); //Checks for json/Psych Engine dialogue
